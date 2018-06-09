9577378e9c9474fa5b06d8ca1b2928dee9bc48f1
tuvanthuan1996@gmail.com <tuvanthuan1996@gmail.com>
2018-06-09 10:34:43 +0700

    Sua file

--- SellWatches/src/main/java/j2ee/spring/sellwatches/services/TrademarkService.java
+++
@@ -1,9 +0,0 @@
-package j2ee.spring.sellwatches.services;
-
-import j2ee.spring.sellwatches.models.Account;
-import j2ee.spring.sellwatches.models.Trademark;
-
-public interface TrademarkService extends MSSQLService<Trademark> {
-
-	
-}
