99bee738c51fa41cdf76d670bc5c41b760350b2d
tuvanthuan1996@gmail.com <tuvanthuan1996@gmail.com>
2018-06-09 12:27:13 +0700

    Thuận
    Login page hoàn thành
    
    Co-Authored-By: Thuan Saraiva <thuan@users.noreply.github.com>

---
+++ SellWatches/src/main/java/j2ee/spring/sellwatches/controllers/AdminAccountController.java
@@ -0,0 +1,52 @@
+package j2ee.spring.sellwatches.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import j2ee.spring.sellwatches.common.CommonConstands;
+import j2ee.spring.sellwatches.common.Encryptor;
+import j2ee.spring.sellwatches.services.AccountService;
+import j2ee.spring.sellwatches.viewmodel.AdminLoginViewModel;
+
+@Controller
+@Component
+@SessionAttributes(CommonConstands.USER_SESSION)
+public class AdminAccountController {
+	
+	@Autowired 
+	AccountService service;
+	
+	@RequestMapping(value = "admin/login", method = RequestMethod.GET)
+	public String index(Model model)
+	{
+		model.addAttribute("account", new AdminLoginViewModel());
+		return "admin_login";
+	}
+	@RequestMapping(value = "admin/login", method = RequestMethod.POST)
+	public String login(@ModelAttribute("account") AdminLoginViewModel account)
+	{
+		// Láº¥y dá»¯ liá»‡u
+		String userName = account.getUsername();
+		String password = account.getPassword();
+		// kiá»ƒm tra
+		if(!userName.toLowerCase().equals("admin"))
+		{
+			account.setError("Tài khoản mật khẩu không đúng!");
+			return "admin_login";
+		}
+		// xá»¯ lÃ½
+		boolean checkLogin = service.LoginAccount(userName, Encryptor.MD5Hash(password));
+		if(!checkLogin)
+		{
+			account.setError("Tài khoản mật khẩu không đúng!");
+			return "admin_login";
+		}
+		return "redirect:home";
+	}
+}
